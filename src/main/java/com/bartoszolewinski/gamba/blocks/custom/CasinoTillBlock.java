3bf801de3c9e9153d6fe042cc395db9d9ef88692
bartoszOlewinski <bartosz.olewinski@gmail.com>
2025-07-16 21:16:04 +0200

    Readded custom class for casino till for future

--- src/main/java/com/bartoszolewinski/gamba/blocks/custom/CasinoTillBlock.java
+++ src/main/java/com/bartoszolewinski/gamba/blocks/custom/CasinoTillBlock.java
@@ -1,2 +1,11 @@
-package com.bartoszolewinski.gamba.blocks.custom;public class CasinoTillBlock {
+package com.bartoszolewinski.gamba.blocks.custom;
+
+import net.minecraft.world.level.block.Block;
+
+public class CasinoTillBlock extends Block {
+
+
+    public CasinoTillBlock(Properties properties) {
+        super(properties);
+    }
 }
