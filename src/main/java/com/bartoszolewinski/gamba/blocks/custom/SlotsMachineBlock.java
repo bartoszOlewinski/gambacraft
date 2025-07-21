44f2ee14faf15c68fdcb5d07b02f02306559ef94
bartoszOlewinski <bartosz.olewinski@gmail.com>
2025-07-21 19:29:07 +0200

    Added custom voxel shape to the slots machine block

--- src/main/java/com/bartoszolewinski/gamba/blocks/custom/SlotsMachineBlock.java
+++ src/main/java/com/bartoszolewinski/gamba/blocks/custom/SlotsMachineBlock.java
@@ -1,11 +1,15 @@
 package com.bartoszolewinski.gamba.blocks.custom;
 
 import com.mojang.serialization.MapCodec;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 import javax.annotation.Nullable;
 
@@ -13,9 +17,16 @@ public class SlotsMachineBlock extends HorizontalDirectionalBlock {
 
     public static final MapCodec<CasinoTillBlock> CODEC = simpleCodec(CasinoTillBlock::new);
 
+    private static final VoxelShape SHAPE = Block.box(3, 0, 1, 14 ,27, 14);
+
 
     public SlotsMachineBlock(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return SHAPE;
     }
 
     @Override
